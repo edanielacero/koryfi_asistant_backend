6a416e475c69ee2c83669ed6742f45d4d18fe7e5
daniel acero <edanielacero@gmail.com>
2024-10-26 15:30:18 -0400

    trying get

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ app.post('/api/chatbot', async (req, res) => {
   }
 });
 
+// Ruta para mostrar algo en el navegador
+app.get('/', (req, res) => {
+  res.send('Bienvenido al servidor del chatbot');
+});
+
 app.listen(port, () => {
   console.log(`Servidor Node.js corriendo en el puerto ${port}`);
 });
