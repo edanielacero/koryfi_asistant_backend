ce5ea7e4801c65ad5454f8b73d6a160831371619
daniel acero <edanielacero@gmail.com>
2024-10-26 15:59:22 -0400

    just railway port in const

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5003;
+const port = process.env.PORT;
 const axios = require('axios');
 
 app.use(cors({
